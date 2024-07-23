2981ca80c8d7bd71114748bff15b108d49357457
Manuel S <156262208+elsantibanez@users.noreply.github.com>
2024-07-23 09:18:16 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,14 @@ set -e
 # GitHub user info
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
-git config --global --add safe.directory /github/workspace # This is to avoid the error: "error: refusing to lose untracked file at 'xxx'"
+git config --global --add safe.directory /github/workspace
 
-# Set up any environment variables if needed
-# export PYTHONPATH=/app
+# Print environment variables for debugging
+echo "GITHUB_ACTOR: ${GITHUB_ACTOR}"
+echo "INPUT_EMAIL: ${INPUT_EMAIL}"
 
 # Run the Python script
-python3 /usr/bin/travel_calculator.py
+python3 /app/travel_calculator.py
+
+# Execute any additional commands passed to the container
+exec "$@"
